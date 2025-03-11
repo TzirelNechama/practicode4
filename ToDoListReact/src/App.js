fe8a028dafb333f155f097a9495e27a4f8c58a62
Tzirel Nechama <tn0583276640@gmail.com>
2025-03-11 13:59:58 +0200

    Update App.js

--- ToDoListReact/src/App.js
+++ ToDoListReact/src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Layout from "./components/Layout";
-import rtlPlugin from 'stylis-plugin-rtl';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 import { prefixer } from 'stylis';
 
 const cacheRtl = createCache({
   key: 'muirtl',
-  stylisPlugins: [prefixer, rtlPlugin],
+  stylisPlugins: [prefixer],
 });
 
 function App() {
